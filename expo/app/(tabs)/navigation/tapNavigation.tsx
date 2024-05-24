5a826f7d6c57b64efb4614705db64f3f0bbff3f0
lioba00700 <lioba00700@naver.com>
2024-05-24 10:25:09 +0900

    하단 네비게이션 수정

--- expo/app/(tabs)/navigation/tapNavigation.tsx
+++ expo/app/(tabs)/navigation/tapNavigation.tsx
@@ -1,69 +1,58 @@
+import React from 'react';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Main_Screen from '../main_screen';
 import Goal_Screen from '../goal_screen';
 import TogetherScreen from '../TogetherScreen';
 import Diary from '../Diary';
-import React from 'react';
-import { Text } from 'react-native';
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 const TapNavigation = () => {
     return (
-        <Tab.Navigator initialRouteName="Home"> 
+        <Tab.Navigator initialRouteName="Home">
           <Tab.Screen 
             name="Home" 
             component={Main_Screen} 
-            options={{ headerShown: false, tabBarIcon: ({color, size}) => (
-              <Text>홈</Text>
-            ),}}
-            // options={{
-            //   tabBarIcon: ({ color, size }) => (
-            //     <Icon name="home-outline" color={color} size={size} />
-            //   ),
-            // }}
+            options={{ 
+              headerShown: false,
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="home-outline" color={color} size={size} />
+              ),
+            }}
           />
           <Tab.Screen 
             name="Goal" 
             component={Goal_Screen} 
-            options={{ headerShown: false, tabBarIcon: ({color, size}) => (
-              <Text>목표</Text>
-            ),}}
-            // options={{
-            //   tabBarIcon: ({ color, size }) => (
-            //     <Icon name="trophy-outline" color={color} size={size} />
-            //   ),
-            // }}
+            options={{ 
+              headerShown: false,
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="trophy-outline" color={color} size={size} />
+              ),
+            }}
           />
           <Tab.Screen 
             name="Together" 
             component={TogetherScreen} 
-            options={{ headerShown: false, tabBarIcon: ({color, size}) => (
-              <Text>공유</Text>
-            ),}}
-            // options={{
-            //   tabBarIcon: ({ color, size }) => (
-            //     <Icon name="trophy-outline" color={color} size={size} />
-            //   ),
-            // }}
+            options={{ 
+              headerShown: false,
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="people-outline" color={color} size={size} />
+              ),
+            }}
           />
           <Tab.Screen 
             name="Diary" 
             component={Diary} 
-            options={{ headerShown: false, tabBarIcon: ({color, size}) => (
-              <Text>일기</Text>
-            ),}}
-            // options={{
-            //   tabBarIcon: ({ color, size }) => (
-            //     <Icon name="trophy-outline" color={color} size={size} />
-            //   ),
-            // }}
+            options={{ 
+              headerShown: false,
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="book-outline" color={color} size={size} />
+              ),
+            }}
           />
-          
         </Tab.Navigator>
-      );
-  }
+    );
+}
 
 export default TapNavigation;
-
-
